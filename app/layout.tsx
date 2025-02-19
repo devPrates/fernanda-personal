4bfa1436bbf60fc88ded7dc3ed80783c0f6f355b
Gabriel Prates Bitencourt <devprates@gmail.com>
2025-02-18 21:33:10 -0400

    feat: Project setup

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Poppins  } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
+
+
+
+export const metadata: Metadata = {
+  title: "Fernanda Prates Bitencourt",
+  description: "Personal Trainer Fernanda Prates Bitencourt",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="PT-BR">
+      <body
+        className={`${poppins.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
